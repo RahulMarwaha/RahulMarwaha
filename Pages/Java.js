11b544b2676ccb4053903106a96d5515c9010ab1
RahulMarwaha <54942870+RahulMarwaha@users.noreply.github.com>
2019-10-04 10:59:22 -0400

    10/4/2019

---
+++ Pages/Java.js
@@ -0,0 +1,10 @@
+function Angles(){
+	var user_angle1= prompt("Enter your first Angle!");
+	var user_angle2= prompt("Enter your second Angle!");
+	if(180 - user_angle1 -user_angle2 >=0){
+		prompt("Your angle is:")
+		alert("The angle is:" 180-user_angle1 - user_angle2)
+		
+	}
+
+}
