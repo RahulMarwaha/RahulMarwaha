09822121a308cbbb553315a7cc7a01c98528a877
RahulMarwaha <54942870+RahulMarwaha@users.noreply.github.com>
2019-10-04 20:56:16 -0500

    2019-10-04

--- Pages/Java.js
+++ Pages/Java.js
@@ -2,9 +2,16 @@ function Angles(){
 	var user_angle1= prompt("Enter your first Angle!");
 	var user_angle2= prompt("Enter your second Angle!");
 	if(180 - user_angle1 -user_angle2 >=0){
-		prompt("Your angle is:")
-		alert("The angle is:" 180-user_angle1 - user_angle2)
+		alert(180-user_angle1 - user_angle2)
 		
 	}
 
+}
+function Angle(){
+		firstangle = document.getElementById("firstangle").value;
+		secondangle = document.getElementById("secondangle"). value;
+		document.getElementById("result").innerHTML = 180-firstangle-secondangle
+	if (180-firstangle-secondangle <=0) {
+		 var result = alert("NaN")
+	}
 }
