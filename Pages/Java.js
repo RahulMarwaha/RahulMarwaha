8bd0c0599798022c7db4db45f137c172c8ee79fb
RahulMarwaha <54942870+RahulMarwaha@users.noreply.github.com>
2019-10-11 12:37:30 -0500

    2019/10/11

--- Pages/Java.js
+++ Pages/Java.js
@@ -5,29 +5,37 @@ function Angles(){
 		alert(180-user_angle1 - user_angle2)
 		
 	}
-
+	if(180-user_angle1-user_angle2 <=0){
+		alert("This can not be done. Please put two other angles.")
+	}
 }
-	function maths(){
-			var firstangle,text;
-			firstangle
-			=document.getElementById("user_angle1"). value;
-		if (firstangle<0 && firstangle>180){
-			var secondangle, text;
-			secondangle
-			=document.getElementById("user_angle2").value;}
-		if(secondangle<0 && secondangle>180){
-			alert("NaN")
-		}		
-		else(true)
-			document.getElementById("result").text;
-		
-		}
+
 function Years(){
 	var user_century= prompt("What is the year you want?");
 	if(user_century%1000 ==0){
 		alert(user_century/100)
 		}
+	if(user_century>=0 && user_century<= 100){
+		alert("1");
+	}
 	else{
-		alert(Math.round(user_century / 100 + 1 ))
+		alert(Math.trunc(user_century / 100 + 1 ))
 		}
 	}
+	//Math.trunc was used to take away the decimal numbers created from dividing the users year by 100//
+function Numbers(){
+for(var i=1; i<=100; i++){
+	if(i%4 ===0 && i%10 ===0){
+		console.log("TenFour!")
+	}
+	else if(i%10 ===0){
+		console.log("ten")
+	}
+	else if(i%4 ===0){
+		console.log("four")
+	}
+	else{
+		console.log(i);
+	}
+}
+}
